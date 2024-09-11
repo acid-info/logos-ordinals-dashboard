afbdae2658cfe2497417b3299cc4c6ab8814180a
jinhojang6 <jinho5051@gmail.com>
2024-09-12 03:14:25 +0900

    chore: update site title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppLayoutProps) {
   return (
     <LSDThemeProvider>
       <Head>
-        <title>Logos NextJS Starter</title>
+        <title>Logos Ordianals</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
