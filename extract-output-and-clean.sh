8076c5410e66bf00b436a228e4638ef7157bee00
status-im-auto <auto@status.im>
2024-12-18 18:20:20 +0000

    Update documentation

--- extract-output-and-clean.sh
+++ extract-output-and-clean.sh
@@ -7,16 +7,9 @@ set -x
 if [ -f "out.tar.gz" ]; then
     echo "Found tar file"
 
-    if /usr/bin/tar -xzf "out.tar.gz" -C .; then
+    if /usr/bin/tar -xzf "out.tar.gz" -C $(pwd) --strip-components=1; then
         echo "Successfully extracted tar file"
 
-        if /usr/bin/rm -f "out.tar.gz"; then
-            echo "Successfully cleaned up tar file"
-            exit 0
-        else
-            echo "Failed to clean up tar file"
-            exit 1
-        fi
     else
         echo "Failed to extract tar file"
         exit 1
